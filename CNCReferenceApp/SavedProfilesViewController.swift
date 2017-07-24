e9bbc679b0c2300e79d5fd34f61c934f3c83f40a
Kale Baiton <kalebaiton@Kales-MacBook-Pro.local>
2017-07-23 18:22:40 -0600

    creating viewcontrollers and groups

---
+++ CNCReferenceApp/SavedProfilesViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SavedProfilesViewController.swift
+//  CNCReferenceApp
+//
+//  Created by Kale Baiton on 2017-07-23.
+//  Copyright © 2017 Kale Baiton. All rights reserved.
+//
+
+import UIKit
+
+class SavedProfilesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
