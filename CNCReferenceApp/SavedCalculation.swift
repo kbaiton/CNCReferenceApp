63fb2ba1bad629e5aa83ae692fb0a96841835833
Kale Baiton <kalebaiton@Kales-MacBook-Pro.local>
2017-08-14 12:40:53 -0600

    adding saved calc model

---
+++ CNCReferenceApp/SavedCalculation.swift
@@ -0,0 +1,20 @@
+//
+//  SavedCalculation.swift
+//  CNCReferenceApp
+//
+//  Created by Kale Baiton on 2017-08-14.
+//  Copyright © 2017 Kale Baiton. All rights reserved.
+//
+
+import Foundation
+
+class SavedCalculation: NSObject {
+    
+    var name: NSString = ""
+    var bitDiameter: NSNumber = 0
+    var spindleSpeed: NSNumber = 0
+    var numberOfFlutes: NSNumber = 0
+    var chipLoad: NSNumber = 0
+    var feedRate: NSNumber = 0
+    
+}
