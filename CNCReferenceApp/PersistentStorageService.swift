41baef4a03ed8067eb0890ae12b398be6dc82cfc
Kale Baiton <kalebaiton@Kales-MacBook-Pro.local>
2017-08-14 12:48:20 -0600

    adding persistent storage class

---
+++ CNCReferenceApp/PersistentStorageService.swift
@@ -0,0 +1,21 @@
+//
+//  PersistentStorageService.swift
+//  CNCReferenceApp
+//
+//  Created by Kale Baiton on 2017-08-14.
+//  Copyright © 2017 Kale Baiton. All rights reserved.
+//
+
+import Foundation
+
+class PersistentStorageService {
+    
+    static func getSavedCalculations() -> [SavedCalculation] {
+        return []
+    }
+    
+    static func setSavedCalculations(_ calculations: [SavedCalculation]) {
+        //implement save
+    }
+    
+}
