4a18994aafe9be7fd7678de4a63b4d677168b9fe
Kale Baiton <kalebaiton@Kales-MacBook-Pro.local>
2017-08-24 08:20:58 -0600

    making calculator vm tests

---
+++ CNCReferenceAppTests/CalculatorViewModelTests.swift
@@ -0,0 +1,36 @@
+//
+//  CalculatorViewModelTests.swift
+//  CalculatorViewModelTests
+//
+//  Created by Kale Baiton on 2017-07-23.
+//  Copyright © 2017 Kale Baiton. All rights reserved.
+//
+
+import XCTest
+@testable import CNCReferenceApp
+
+class CalculatorViewModelTests: XCTestCase {
+    
+    var sut: CalculatorViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        sut = CalculatorViewModel()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testCalculatorCellsInitalizedProperly() {
+        XCTAssert(sut.cellModels.array.contains(where: { $0.type == .bitDiameter }))
+        XCTAssert(sut.cellModels.array.contains(where: { $0.type == .spindleSpeed }))
+        XCTAssert(sut.cellModels.array.contains(where: { $0.type == .flutes }))
+        XCTAssert(sut.cellModels.array.contains(where: { $0.type == .chipLoad }))
+        XCTAssert(sut.cellModels.array.contains(where: { $0.type == .feedRate }))
+    }
+    
+}
